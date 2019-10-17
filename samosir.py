7c8bdb7cc35b014dc45a4d66165999481abfc120
Nicoleus1 <55134353+nicoleus@users.noreply.github.com>
2019-10-17 14:47:33 +0700

    samosir.py

--- samosir.py
+++ samosir.py
@@ -168,8 +168,6 @@ class shit3:
       print green+"([2]} Samosir "+c+"To "+g+" Batak"
       self.sug(w+'-'*42)
       print green+"([00])} Back..HORAS.."
-      self.sug(w+'-'*42)
-      print green+"([##])} HORAS HITA SUDE..."+c+"To "+g+"Samosir
 
  
 
